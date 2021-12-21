f3bfdaf6a77d2f07762fb6c028749ac334918d93
Arvinder <30565401+arvinder-dhar@users.noreply.github.com>
2021-12-22 01:02:25 +0530

    Update AzureWebapp.py

--- AzureWebapp.py
+++ AzureWebapp.py
@@ -1,3 +1,15 @@
+'''
+
 def hello(request):
     print("Handling request to home page.")
     return HttpResponse("Hello, Arvinder!")
+'''
+
+pip install flask
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!\n"
